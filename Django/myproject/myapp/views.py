0f1f475956cf2d0a2a5077c2527e3e6f6e4627bd
AndresP1102 <89894189+AndresP1102@users.noreply.github.com>
2023-06-25 02:29:40 -0500

    Progress

--- Django/myproject/myapp/views.py
+++ Django/myproject/myapp/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Welcome to Little Lemon!")
+
+def drinks(request, drink_name):
+    drink = {
+        "mocha": "Coffee",
+        "tea":"Bevarage",
+        "lemonade":"Refreshment",
+    }
+    choice_of_drink = drink[drink_name]
+
+    return HttpResponse(f"<h2> {drink_name} <h2>" + choice_of_drink)
